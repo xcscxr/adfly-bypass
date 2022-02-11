83e8d8bf2a8b6ef9481b20cc508c30a5e0051d20
Yuuki <82380343+xcscxr@users.noreply.github.com>
2022-02-11 06:12:17 +0530

    init

---
+++ adfly_bypass.py
@@ -0,0 +1,80 @@
+import re
+import requests
+from base64 import b64decode
+from urllib.parse import unquote
+
+# ==========================================
+
+# adfly short url
+URL = ""
+
+'''
+404: Complete exception handling not found :(
+'''
+
+# ==========================================
+
+def decrypt_url(code):
+    a, b = '', ''
+    for i in range(0, len(code)):
+        if i % 2 == 0: a += code[i]
+        else: b = code[i] + b
+
+    key = list(a + b)
+    i = 0
+
+    while i < len(key):
+        if key[i].isdigit():
+            for j in range(i+1,len(key)):
+                if key[j].isdigit():
+                    u = int(key[i]) ^ int(key[j])
+                    if u < 10: key[i] = str(u)
+                    i = j					
+                    break
+        i+=1
+    
+    key = ''.join(key)
+    decrypted = b64decode(key)[16:-16]
+
+    return decrypted.decode('utf-8')
+
+# ==========================================
+
+def adfly_bypass(url):
+    res = requests.get(url).text
+    
+    out = {'error': False, 'src_url': url}
+    
+    try:
+        ysmm = re.findall("ysmm\s+=\s+['|\"](.*?)['|\"]", res)[0]
+    except:
+        out['error'] = True
+        return out
+        
+    url = decrypt_url(ysmm)
+
+    if re.search(r'go\.php\?u\=', url):
+        url = b64decode(re.sub(r'(.*?)u=', '', url)).decode()
+    elif '&dest=' in url:
+        url = unquote(re.sub(r'(.*?)dest=', '', url))
+    
+    out['bypassed_url'] = url
+    
+    return out
+
+# ==========================================
+
+out = adfly_bypass(URL)
+
+print(out)
+
+# ==========================================
+
+'''
+SAMPLE OUTPUT:
+{
+    'error': False, 
+    'src_url': 'http://fumacrom.com/1u0t', 
+    'bypassed_url': 'https://google.com'
+}
+'''
